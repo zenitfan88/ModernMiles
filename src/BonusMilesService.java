a30a27d2c1b5eb37b723bc4482725479e1bd5504
zenitfan88 <nikanenkov@rambler.ru>
2022-06-13 18:59:23 +0400

    Modifiel code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,14 @@
 public class BonusMilesService {
-    public int calculate(int price, int cost) {
+    // 1 вариант. Придерживаемся условия задачи.
+    public int calculate(int cost) {
+        int price = 10_000;
         int miles = price / cost;
         return miles;
+
+// 2 вариант. Придерживаемся кода программы
+    /*public int calculate(int price) {
+        int cost = 20;
+        int miles = price / cost;
+        return miles;*/
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
+    // 1 вариант. Придерживаемся условия задачи.
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int miles = service.calculate(10_000, 20);
+        int miles = service.calculate(20);
         System.out.println(miles);
+
+        // 2 вариант. Придерживаемся кода программы
+     /*public static void main(String[] args) {
+         BonusMilesService service = new BonusMilesService();
+         int price = 10_000;
+         int miles = service.calculate(price);
+         System.out.println(miles);*/
     }
 }
+
