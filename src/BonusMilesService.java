58488d6ff95c814698b87afe00979efedbf25895
zenitfan88 <nikanenkov@rambler.ru>
2022-06-14 16:33:32 +0400

    Fixed

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,14 +1,7 @@
 public class BonusMilesService {
-    // 1 вариант. Придерживаемся условия задачи.
-    public int calculate(int cost) {
-        int price = 10_000;
-        int miles = price / cost;
-        return miles;
-
-// 2 вариант. Придерживаемся кода программы
-    /*public int calculate(int price) {
+    public int calculate(int price) {
         int cost = 20;
         int miles = price / cost;
-        return miles;*/
+        return miles;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
 public class Main {
-    // 1 вариант. Придерживаемся условия задачи.
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int miles = service.calculate(20);
+        int price = 10_000;
+        int miles = service.calculate(price);
         System.out.println(miles);
-
-        // 2 вариант. Придерживаемся кода программы
-     /*public static void main(String[] args) {
-         BonusMilesService service = new BonusMilesService();
-         int price = 10_000;
-         int miles = service.calculate(price);
-         System.out.println(miles);*/
     }
 }
 
